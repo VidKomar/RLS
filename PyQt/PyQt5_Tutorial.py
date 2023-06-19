1a1cf9e1bec481a012baa247f9adb68a71315511
DemonLama <vid.komar@gmail.com>
2023-06-19 17:22:28 +0200

    GitHub Desktop App Commit
    Let's see, how big of a mistake this is.

--- PyQt/PyQt5_Tutorial.py
+++ PyQt/PyQt5_Tutorial.py
@@ -1,12 +1,64 @@
 # https://build-system.fman.io/pyqt5-tutorial
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QListView
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel, QListView,
+                             QComboBox, QWidget, QVBoxLayout, QMessageBox)
 
+import sys
 
-app = QApplication([]) # No parameters used, as of yet
-label = QLabel('Hello World!')
-label.show()
+class ComboBox(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-app.exec() # Starting the event loop. exec_ is archaic.
+    def initUI(self):
+        layout = QVBoxLayout() # Vertical layout
+
+        label = QLabel("Select an option:") # Label for Combobox instructions
+        layout.addWidget(label)
+
+        combobox = QComboBox() #Different options
+        combobox.addItem("Choose and option (not me)")
+        combobox.addItem("Option 1")
+        combobox.addItem("Option 2")
+        combobox.addItem("Option 3")
+
+        combobox.currentIndexChanged.connect(self.ComboBoxChange)  # Call of function that notices the change
+        layout.addWidget(combobox)
+
+        #layout.addWidget(QPushButton("Submit"))
+        button = QPushButton("Submit choice") # Button for "submission"
+        button.clicked.connect(self.submitData)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+        self.setWindowTitle("Example of Combobox")
+        self.show()
+
+    def ComboBoxChange(self, index):
+        """
+        Get's the information about the clicked entry on the combo box.
+        """
+        combobox = self.sender() 
+        global selected_item # I've got a feeling that this is bad practice
+        selected_item = combobox.currentText()
+        print("Selected item: {}".format(selected_item))
+
+    def submitData(self, index):
+        """
+        Displays a message box upon pressing the submit button.
+        The user is notified of the choice he made.
+        """
+        alert = QMessageBox()
+        alert.setText("You clicked the button and selected: \n {}".format(selected_item))
+        alert.setWindowTitle("Click Conformation")
+        alert.exec()
+
+if __name__ == "__main__":
+
+    app = QApplication([]) # No parameters used, as of yet
+    ex = ComboBox()
+    sys.exit(app.exec()) # Starting the event loop. exec_ is archaic.
 
 # Adding widgets
+# Also some issues with git hah
+
---
+++ Test.py
@@ -0,0 +1,37 @@
+#http://agromet.mkgp.gov.si/APP2/sl/Home/Index?id=2&archive=0&graphs=1#esri_map_iframe
+import sys
+from PyQt5.QtCore import QUrl
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest
+from PyQt5.QtWidgets import QApplication
+
+app = QApplication(sys.argv)
+
+# Create a QNetworkAccessManager instance
+manager = QNetworkAccessManager()
+
+# Define the URL
+url = QUrl("http://agromet.mkgp.gov.si/APP2/sl/Home/Index?id=2&archive=0&graphs=1&xml=1")
+
+# Create a QNetworkRequest object and set the URL
+request = QNetworkRequest(url)
+
+# Send a GET request to the URL
+reply = manager.get(request)
+
+# Callback function to handle the reply
+def handle_reply():
+    if reply.error() == QNetworkAccessManager.NoError:
+        # Read the XML content from the reply
+        xml_content = reply.readAll().data().decode()
+
+        # Process the XML content
+        # Here, you can use an XML parser like xml.etree.ElementTree to extract the data
+        print(xml_content)
+    else:
+        print("Request failed with error:", reply.errorString())
+
+# Connect the finished signal of the reply to the handle_reply function
+reply.finished.connect(handle_reply)
+
+# Execute the event loop
+sys.exit(app.exec_())
