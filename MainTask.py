8e86921cb958e974458fd66d994f96e0b4773d99
DemonLama <vid.komar@gmail.com>
2023-06-20 23:21:15 +0200

    Function to pull temperature data from URL.     Currently takes one URL of XML.     Creates a dictionary with temperatures and time.     Breaks upon completion. Created actual (simple) GUI for selection of data. Only temperatures can be selected (FOR NOW). Conformation with a button. Selected temperature is plotted. (WIP)

---
+++ MainTask.py
@@ -0,0 +1,58 @@
+import requests
+import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import QApplication
+import sys
+
+
+# Send a GET request to the XML file URL
+url = 'http://agromet.mkgp.gov.si/APP2/AgrometContent/xml/55.xml'
+
+# TEMPS tavg = avg temp.; tx = max temp; tn = min temp.
+# HUMIDITY rhavg, rhx, rhn
+#  
+def pullData(url):
+    response = requests.get(url)
+    if response.status_code == 200: # Check if the request was successful
+        root = ET.fromstring(response.content) # Parse the XML content
+
+        # Extract temperature data from the XML
+        data_dict = {}
+        entries = 10
+        for element in root.iter():
+            #if element.tag in ["tavg"]: # Test purpose - only show one temp.
+            if element.tag in ["valid_UTC", "tavg", "tx", "tn"]: # Finding temperature markers
+                tag = element.tag # Parse tags and values from XML
+                value = element.text
+
+                if tag in data_dict:
+                    data_dict[tag].append(value) # Add value to tag
+                else:
+                    data_dict[tag] = [value]
+                
+                #if len(data_dict["tavg"]) >= entries: # Limiting entries for testing
+                #    break
+                if element == ("</data>"): # When end is reached, break
+                    break
+        return data_dict
+
+        """ # Create the graph using matplotlib
+        # 3 different temperatures
+        plt.subplot(3, 1, 1)
+        plt.plot(data_dict.get("tavg"))
+        plt.subplot(3, 1, 2)
+        plt.plot(data_dict.get("tx"))
+        plt.subplot(3, 1, 3)
+        plt.plot(data_dict.get("tn"))
+        plt.xlabel('Time')
+        plt.ylabel('Temperature (°C)')
+        plt.title('Temperature Variation')
+        plt.show()"""
+
+        # Start the PyQt5 event loop
+        #app = QApplication([])
+        #app.exec_()
+        #sys.exit(app.exec())
+    else:
+        print(f'Request failed with status code: {response.status_code}')
+
--- TaskGUI.py
+++ TaskGUI.py
@@ -2,29 +2,81 @@ from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel, QLi
                              QComboBox, QWidget, QVBoxLayout, QMessageBox)
 
 from PyQt5.QtCore import QSize, Qt, QStringListModel
-
+import matplotlib.pyplot as plt
 import sys
+import MainTask
+
+def displaySelected(selected_infoIdx, data_dict):
+        if selected_infoIdx == 0:
+            plt.plot(data_dict.get("tavg"))
+            plt.show()
+        if selected_infoIdx == 1:
+            plt.plot(data_dict.get("tx"))
+            plt.show()
+        if selected_infoIdx == 2:
+            plt.plot(data_dict.get("tn"))
+            plt.show()
+        else:
+            plt.plot() # Empty
+            plt.show
+        
 
 class MainW(QWidget):
     def __init__(self):
         super().__init__()
-        #self.initUI()
+        self.initUI()
 
+    def initUI(self):
+        layout = QVBoxLayout()
+        # Setting small size and name
         self.setFixedSize(400, 300)
         self.setWindowTitle("Weather app.")
 
-        layout = QVBoxLayout()
-
-        label = QLabel("Welcome to my weather app")
-        label.setAlignment(Qt.AlignTop)
+        # Greeting and instructions on top
+        label = QLabel("Welcome to my weather app \n"
+                       "Please choose information you are interested in!")
+        label.setAlignment(Qt.AlignCenter)
         layout.addWidget(label) 
+        
+        # Choose the desired display output
+        displayChoice = QComboBox()
+        displayChoice.addItem("Average temperature")
+        displayChoice.addItem("Maximum temperature")
+        displayChoice.addItem("Minimum temperature")
+        layout.addWidget(displayChoice)
+
+        # Confirm choice of display output
+        buttonConfirm = QPushButton("Confirm Choice")
+        buttonConfirm.clicked.connect(self.buttonConfirm)
+        layout.addWidget(buttonConfirm)
+
 
         self.setLayout(layout)     
 
         self.show()
 
-    #def initUI(self):
-        
+    def buttonConfirm(self):
+        """
+        Upon button conformation, pull data.
+        Model and View are seperated.
+        return: 
+            selected_info - Which information, the user wishes to see.
+            data_dict - Pulled information from the source webpage xml file.
+        """
+        displayChoice = self.sender().parent().findChild(QComboBox)
+        selected_infoIdx = displayChoice.currentIndex()
+        print(selected_infoIdx)
+
+        # Pull data from website using MainTask function
+        data_dict = MainTask.pullData("http://agromet.mkgp.gov.si/APP2/AgrometContent/xml/55.xml")
+
+        displaySelected(selected_infoIdx, data_dict)
+
+        return selected_infoIdx, data_dict
+
+    
+
+#displaySelected(selected_info, data_dict)
 
 if __name__ == "__main__":
 
