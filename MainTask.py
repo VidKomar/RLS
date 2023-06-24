6dd1716a1954eda3e18f62f39ba2dd654fa2d3f7
DemonLama <vid.komar@gmail.com>
2023-06-24 17:29:19 +0200

    Fixing some branching issues.

--- MainTask.py
+++ MainTask.py
@@ -17,41 +17,31 @@ def pullData(url):
         # Extract temperature data from the XML
         data_dict = {}
         for element in root.iter():
-            tag = element.tag  # Parse tags and values from XML
-            value = element.text
-
-            if tag in data_dict:
-                data_dict[tag].insert(0, value)
-            else:
-                data_dict[tag] = [value]
-
             # Finding temperature, humidity,
             # rainfall, dew point temp., Leaf Wetness markers
-            """if element.tag in ["valid_UTC", "tavg", "tx", "tn",
+            if element.tag in ["valid_UTC", "tavg", "tx", "tn",
                                 "rhavg", "rhx", "rhn", 
-                                "td", "rr", "lwavg"]:"""
-                  
-            """tag = element.tag  # Parse tags and values from XML
-            value = element.text
+                                "td", "rr", "lwavg"]:  
+                tag = element.tag  # Parse tags and values from XML
+                value = element.text
 
-            if tag in data_dict:
-                data_dict[tag].insert(0, value)  # Add value to tag
-            else:
-                data_dict[tag] = [value]"""
-            
-        
-            # Two days worth of data is required...
-            # timeOfData = []
-            # timeOfData = timeOfData.append(str(data_dict["valid_UTC"[11:15]]))
-            # timeTemp = data_dict["valid_UTC"]
-            # timeOfData = timeOfData.append(str(timeTemp[11:15]))
+                if tag in data_dict:
+                    data_dict[tag].insert(0, value)  # Add value to tag
+                else:
+                    data_dict[tag] = [value]
+                
+                # Two days worth of data is required...
+                # timeOfData = []
+                # timeOfData = timeOfData.append(str(data_dict["valid_UTC"[11:15]]))
+                # timeTemp = data_dict["valid_UTC"]
+                # timeOfData = timeOfData.append(str(timeTemp[11:15]))
 
-            # if len(data_dict["tavg"]) >= entries: # Limiting entries for testing
-            #    break
-            if element == ("</data>"):  # When end is reached, break
-                break
-        #data_dict.update({"Units":"None", "°C", "°C", "°C", "%", "%", "%", "°C", "mm", "None"})
+                # if len(data_dict["tavg"]) >= entries: # Limiting entries for testing
+                #    break
+                if element == ("</data>"):  # When end is reached, break
+                    break
         return data_dict
+
 
         # Start the PyQt5 event loop
         # app = QApplication([])
