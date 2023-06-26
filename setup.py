be8aedf090426fc6aa57f80a7f25b37ce08e2ea2
VidKomar <vid.komar@gmail.com>
2023-06-26 11:12:28 +0200

    Still Testing PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name='RLS Task GUI',
-    version='0.12dev',
+    name='RLS Task',
+    version='0.13dev',
     packages=['RLS'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     # long_description=open('README.txt').read(),
