e4b342d87d5782916c0245358b3823c529b39921
VidKomar <vid.komar@gmail.com>
2023-06-26 10:43:43 +0200

    Updated version for successful pipeline execution.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='RLS Task',
-    version='0.1dev',
+    version='0.11dev',
     packages=['RLS'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     # long_description=open('README.txt').read(),
