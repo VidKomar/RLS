a92721bb035446289e53220cf35f54e4d86340fd
VidKomar <vid.komar@gmail.com>
2023-06-26 10:49:14 +0200

    Ditto

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name='RLS Task',
-    version='0.11dev',
+    name='RLS Task GUI',
+    version='0.12dev',
     packages=['RLS'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     # long_description=open('README.txt').read(),
