b2f4c74741607e1ffee4b80d18804809a84167bb
DemonLama <vid.komar@gmail.com>
2023-06-28 22:38:51 +0200

    Added for further development after finishing time

---
+++ RLS/TaskGUI.py
@@ -0,0 +1,157 @@
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QLabel,
+    QListView,
+    QComboBox,
+    QWidget,
+    QVBoxLayout,
+    QMessageBox,
+)
+
+from PyQt5.QtCore import QSize, Qt, QStringListModel
+import matplotlib.pyplot as plt
+import sys, logging
+import TaskMain
+
+
+def SimplePlot(data_dict, selectedQuantity):
+
+    """
+    Used for plotting weather variables.
+    Data is gathered from dicts.
+    """
+    temperaturesPlot = [float(x) for x in data_dict.get(selectedQuantity)]
+    timePlot = data_dict.get("valid_UTC")[2:]  # Solve this issue pls
+    # tempPlot = plt.plot(timePlot, temperaturesPlot)
+    fig, ax = plt.subplots(figsize=(15, 8))
+    ax.plot(timePlot, temperaturesPlot)
+    ax.set_ylabel("Applicable unit")  # !!!
+    ax.set_xlabel("Time")
+    fig.autofmt_xdate(rotation=45)
+
+    # Showing every other label
+    plt.setp(ax.get_xticklabels()[::2], visible=False)
+    ax.set_title("Displaying {} in two day span".format(selectedQuantity))
+
+    plt.show()
+
+
+def displaySelected(selected_infoIdx, data_dict):
+    # Instead of all the if, plot by dict. keys!
+    logging.info("TestLog")
+    logging.info(data_dict.keys())
+
+    # Plotting selected information
+    SimplePlot(data_dict, list(data_dict.keys())[selected_infoIdx + 2])
+
+    """  if selected_infoIdx == 0:
+        SimplePlot(data_dict, "tavg")
+    if selected_infoIdx == 1:
+        SimplePlot(data_dict, "tx")
+    if selected_infoIdx == 2:
+        SimplePlot(data_dict, "tn")
+    if selected_infoIdx == 3:
+        SimplePlot(data_dict, "rhavg")
+    if selected_infoIdx == 4:
+        SimplePlot(data_dict, "rhx")
+    if selected_infoIdx == 5:
+        SimplePlot(data_dict, "rhn")
+    if selected_infoIdx == 6:
+        SimplePlot(data_dict, "td")
+    if selected_infoIdx == 7:
+        SimplePlot(data_dict, "rr")
+    if selected_infoIdx == 8:
+        SimplePlot(data_dict, "lwavg")
+    else:
+        plt.plot()  # Empty
+        plt.show"""
+
+
+class MainW(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        layout = QVBoxLayout()
+        # Setting small size and name
+        self.setFixedSize(400, 300)
+        self.setWindowTitle("Weather app.")
+
+        # Greeting and instructions on top
+        label = QLabel(
+            "Welcome to my weather app \n"
+            "Please choose information you are interested in!"
+        )
+        label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label)
+
+        # Choose the desired location
+        displayChoice = QComboBox()
+        displayChoice.addItem("xml.files")
+
+        # Choose the desired display output
+        displayChoice = QComboBox()
+        displayChoice.addItem("Average temperature")
+        displayChoice.addItem("Maximum temperature")
+        displayChoice.addItem("Minimum temperature")
+        displayChoice.addItem("Average relative humidity")
+        displayChoice.addItem("Maximum relative humidity")
+        displayChoice.addItem("Minimum relative humidity")
+        displayChoice.addItem("Dew point temperature")
+        displayChoice.addItem("Rainfall")
+        displayChoice.addItem("Leaf Wetness")
+        layout.addWidget(displayChoice)
+
+        # Confirm choice of display output
+        buttonConfirm = QPushButton("Confirm Choice")
+        buttonConfirm.clicked.connect(self.buttonConfirm)
+        layout.addWidget(buttonConfirm)
+
+        self.setLayout(layout)
+
+        self.show()
+
+    def buttonConfirm(self):
+        """
+        Upon button conformation, pull data.
+        Model and View are seperated.
+        return:
+            selected_info - Which information, the user wishes to see.
+            data_dict - Pulled information from the source webpage xml file.
+        """
+        displayChoice = self.sender().parent().findChild(QComboBox)
+        selected_infoIdx = displayChoice.currentIndex()
+        selected_info = displayChoice.currentText()
+        # print(selected_infoIdx)
+
+        xml_locations = TaskMain.getCitiesData()
+
+        # Selecting from all cities to show data for selection only!
+        # DATA = {}
+        # for i, location in enumerate(xml_locations, start=1):
+        # singleCity = TaskMain.pullData(xml_locations[i])
+        # DATA.update(singleCity)
+
+        # print("Selected city is: {}".format(selectedLocation))
+
+        # Pull data from website using TaskMain function
+        data_dict = TaskMain.pullData(
+            "http://agromet.mkgp.gov.si/APP2/AgrometContent/xml/55.xml"
+        )
+
+        displaySelected(selected_infoIdx, data_dict)
+
+        return selected_infoIdx, data_dict
+
+
+# def physicalEntity():
+
+
+if __name__ == "__main__":
+
+    app = QApplication([])  # No parameters used, as of yet
+    ex = MainW()
+    sys.exit(app.exec())  # Starting the event loop. exec_ is archaic.
---
+++ RLS/TaskMain.py
@@ -0,0 +1,86 @@
+import requests
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+
+# Send a GET request to the XML file URL
+url = "http://agromet.mkgp.gov.si/APP2/AgrometContent/xml/55.xml"
+
+# TEMPS tavg = avg temp.; tx = max temp; tn = min temp.
+# HUMIDITY rhavg, rhx, rhn
+#
+
+
+def pullData(url):
+    response = requests.get(url)
+    if response.status_code == 200:  # Check if the request was successful
+        root = ET.fromstring(response.content)  # Parse the XML content
+
+        # Extract temperature data from the XML
+        data_dict = {}
+        for element in root.iter():
+            # Finding temperature, humidity,
+            # rainfall, dew point temp., Leaf Wetness markers
+            if element.tag in [
+                "valid_UTC",
+                "tavg",
+                "tx",
+                "tn",
+                "rhavg",
+                "rhx",
+                "rhn",
+                "td",
+                "rr",
+                "lwavg",
+                "domain_shortTitle",
+            ]:
+                tag = element.tag  # Parse tags and values from XML
+                value = element.text
+
+                if tag in data_dict:
+                    data_dict[tag].insert(0, value)  # Add value to tag
+                else:
+                    data_dict[tag] = [value]
+
+                # Two days worth of data is required...
+                # timeOfData = []
+                # timeOfData = timeOfData.append(str(data_dict["valid_UTC"[11:15]]))
+                # timeTemp = data_dict["valid_UTC"]
+                # timeOfData = timeOfData.append(str(timeTemp[11:15]))
+
+                # if len(data_dict["tavg"]) >= entries: # Limiting entries for testing
+                #    break
+                if element == ("</data>"):  # When end is reached, break
+                    break
+        return data_dict
+
+        # Start the PyQt5 event loop
+        # app = QApplication([])
+        # app.exec_()
+        # sys.exit(app.exec())
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+
+
+def getCitiesData():
+    """
+    # URL http://agromet.mkgp.gov.si/APP2/sl/Home/Index?id=2&archive=0&graphs=1#esri_map_iframe
+    """
+    # URL of the webpage
+    url = "http://agromet.mkgp.gov.si/APP2/sl/Home/Index?id=2&archive=0&graphs=1#esri_map_iframe"
+
+    response = requests.get(url)
+    # Parse the webpage content using BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Find all anchor tags (links) in the webpage
+    links = soup.find_all("a")
+
+    # Collect all links that end with ".xml"
+    xml_files = []
+    for link in links:
+        if link.has_attr("href") and link["href"].endswith(".xml"):
+            xml_files.append("http://agromet.mkgp.gov.si" + link["href"])
+    # logging.info(xml_files)
+    # print(xml_files)
+
+    return xml_files
